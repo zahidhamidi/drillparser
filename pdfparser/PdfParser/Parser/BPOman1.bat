718f03ed923f98553acdda6832f80e98ad4a2320
Zahid Hamidi Bin Abdul Hamid <ZHamid2@slb.com>
2024-02-16 10:28:44 +0800

    clear sensistive data

--- pdfparser/PdfParser/Parser/BPOman1.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\ZHamid2\Downloads\BPOman\PdfParser\PdfParser.exe -s="C:\Users\ZHamid2\Downloads\BPOman\PdfParser\Parser\BPOman1.json" -i="C:\Users\ZHamid2\Downloads\BPOman\Input" -o="C:\Users\ZHamid2\Downloads\BPOman\Output" -p
-pause
--- pdfparser/PdfParser/Parser/BPOman2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-                C:\Users\ZHamid2\Downloads\BPOman\PdfParser\PdfParser.exe -s="C:\Users\ZHamid2\Downloads\BPOman\PdfParser\Parser\BPOman1.json" -i="C:\Users\ZHamid2\Downloads\BPOman\Input" -o="C:\Users\ZHamid2\OneDrive - SLB\scripts\data_ingestion_DDR\pdfparser\Output" -p
-                pause
-                
